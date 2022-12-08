11009df4bbb1f832f7158812bd636351662a5d72
baidong <you@example.com>
2022-12-08 16:25:22 +0800

    203 移除链表元素（虚节点）

---
+++ src/main/java/leetcode/editor/cn/移除链表元素.java
@@ -0,0 +1,98 @@
+package leetcode.editor.cn;
+
+;
+//给你一个链表的头节点 head 和一个整数 val ，请你删除链表中所有满足 Node.val == val 的节点，并返回 新的头节点 。
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [1,2,6,3,4,5,6], val = 6
+//输出：[1,2,3,4,5]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [], val = 1
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = [7,7,7,7], val = 7
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 列表中的节点数目在范围 [0, 10⁴] 内 
+// 1 <= Node.val <= 50 
+// 0 <= val <= 50 
+// 
+// Related Topics 递归 链表 👍 1101 👎 0
+
+public class 移除链表元素 {
+    public static void main(String[] args) {
+        Solution solution = new 移除链表元素().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode removeElements(ListNode head, int val) {
+            if (head == null) {
+                return null;
+            }
+            ListNode dummy = new ListNode(-1, head);
+            ListNode cur = head;
+            ListNode pre = dummy;
+            while (cur != null) {
+                //直接删除 上一个节点直接对应到下一个节点，跳过本节点
+                if (cur.val == val) {
+                    pre.next = cur.next;
+                }else{
+                    // 没有找到  前节点移动到当前位置，当前节点去查找下一个
+                    pre = cur;
+                }
+                cur = cur.next;
+            }
+            return dummy.next;
+        }
+    }
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
